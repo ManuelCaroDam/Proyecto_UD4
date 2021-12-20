7a15b0a6f3bbeddb7f8fc6df2983e8411305604a
caro.deman20 <caro.deman20@triana.salesianos.edu>
2021-12-20 12:44:37 +0100

    Otros arreglos

--- angular-objetosPerdidos/src/app/components/objetoPerdido-list/objetosPerdidos-list.component.ts
+++ angular-objetosPerdidos/src/app/components/objetoPerdido-list/objetosPerdidos-list.component.ts
@@ -2,20 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ListaObjetosPerdidos } from 'src/app/models/interfaces/objetos-perdidos.interface';
 import { ObjetosPerdidosService } from 'src/app/services/objetoPerdido.service';
 
-
 @Component({
   selector: 'app-objetosPerdidos-list',
   templateUrl: './objetosPerdidos-list.component.html',
-  styleUrls: ['./objetosPerdidos.component.css']
+  styleUrls: ['./objetosPerdidos-list.component.css']
 })
-export class ObjetosPerdidosListComponent implements OnInit {
+export class GasolinerasListComponent implements OnInit {
 
   listaObjetosList: ListaObjetosPerdidos[] = [];
   
-  constructor(private objetoPerdidoService: ObjetosPerdidosService) { }
+  constructor(private objetosPerdidosService: ObjetosPerdidosService) { }
 
   async ngOnInit(): Promise<void> {
-    this.objetoPerdidoService.getObjetoPerdidos().subscribe(resp => {
+    this.objetosPerdidosService.getObjetoPerdidos().subscribe(resp => {
       let jsonString = JSON.stringify(resp);
     });
 
